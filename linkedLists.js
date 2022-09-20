c54b28754d93889176cfd1fa912108f55d8257c5
csturgeon <sturgeon.chris@gmail.com>
2022-09-20 14:54:11 +0100

    Complete extra credit exercises

--- linkedLists.js
+++ linkedLists.js
@@ -65,24 +65,97 @@ class LinkedList {
   atIndex = function (index) {
     try {
       let tempNode = this.head;
-
-      for (let i = 0; i < index; i++) {
+      for (let i = 1; i < index; i++) {
         tempNode = tempNode.nextNode;
       }
 
-      return tempNode.value;
+      return tempNode;
     } catch (error) {
       console.log(
         'Warning: you have selected an index greater than the list length.'
       );
     }
   };
+
+  // Removes the last node from the list
+  pop = function () {
+    if (!this.head) {
+      this.head = null;
+    }
+
+    let previousNode = this.head;
+    let tailNode = this.head.nextNode;
+
+    while (tailNode.nextNode !== null) {
+      previousNode = tailNode;
+      tailNode = tailNode.nextNode;
+    }
+
+    previousNode.tailNode = null;
+  };
+
+  // Returns true if the passed in value is within list.
+  find = function (value) {
+    let tempNode = this.head;
+
+    while (tempNode.nextNode !== null) {
+      if (tempNode.value === value || tempNode.nextNode.value === value) {
+        return true;
+      } else {
+        tempNode = tempNode.nextNode;
+      }
+    }
+    return null;
+  };
+
+  // Console logs all node values as strings
+  toString = function () {
+    if (!this.head) {
+      return null;
+    }
+
+    let tempNode = this.head;
+
+    while (tempNode.nextNode !== null) {
+      console.log(`( ${tempNode.value} ) -> `);
+      tempNode = tempNode.nextNode;
+      if (tempNode.nextNode === null) {
+        console.log(`( ${tempNode.value} ) ->`);
+      }
+    }
+    console.log('null');
+  };
+
+  // Inserts a new node with given value at given index
+  insertAt = function (value, index) {
+    let newNode = new Node(value);
+    let indexNode = this.head;
+
+    for (let i = 1; i < index; i++) {
+      indexNode = indexNode.nextNode;
+    }
+
+    let followingNode = indexNode.nextNode;
+    newNode.nextNode = followingNode;
+    indexNode.nextNode = newNode;
+  };
+
+  // Removes node at given index
+  removeAt = function (index) {
+    if (index <= this.size()) {
+      let indexNode = this.head;
+      let previousNode;
+
+      for (let i = 1; i < index; i++) {
+        previousNode = indexNode;
+        indexNode = indexNode.nextNode;
+      }
+
+      if (index > 1) {
+        previousNode.nextNode = indexNode.nextNode;
+      } else {
+        this.head = indexNode.nextNode;
+      }
+    }
+  };
 }
-
-const myList = new LinkedList();
-myList.prepend('Brian');
-myList.append('Chris');
-myList.append('Eric');
-myList.append('Gail');
-
-console.log(myList.atIndex(3));
